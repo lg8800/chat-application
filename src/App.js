ff3d7cbbf02d5e1835bf9bd21543dafcd504d9c1
DIVANSHMAHAJAN <82203713+DIVANSHMAHAJAN@users.noreply.github.com>
2021-12-29 20:33:27 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import { Switch, Route } from 'react-router-dom';
+import {useContext} from 'react';
+import { Switch, Route ,Redirect} from 'react-router-dom';
 import "./App.css";
 import Layout from './components/Layout/Layout';
-
+import AuthContext from './components/store/auth-context';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
 import ChatPage from './pages/Chat-Page'
 function App() {
+   const authCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
+        <Route path='/chatpage' exact>
+        {authCtx.isLoggedIn && <ChatPage />}
+
+        {!authCtx.isLoggedIn &&<Redirect to='/auth' />}
         </Route>
-         <Route path='/chatpage'>
-          <ChatPage />
+        {!authCtx.isLoggedIn && (
+          <Route path='/auth'>
+            <AuthPage />
+          </Route>
+        )}
+        <Route path='/profile'>
+           
+          {!authCtx.isLoggedIn && <Redirect to='/auth' />}
         </Route>
-        
+        <Route path='*'>
+          <Redirect to='/' />
+        </Route>
       </Switch>
     </Layout>
   );

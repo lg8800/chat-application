bab5eedd311ce1a073a227e19d3f170736e452d2
DIVANSHMAHAJAN <82203713+DIVANSHMAHAJAN@users.noreply.github.com>
2021-12-29 20:32:38 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import { AuthContextProvider } from './components/store/auth-context';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
+	<AuthContextProvider>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </AuthContextProvider>,
   document.getElementById('root')
 );

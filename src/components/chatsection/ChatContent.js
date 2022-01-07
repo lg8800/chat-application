0ea1db4c7572201bf3895898a3e6eea0ab201755
DIVANSHMAHAJAN <82203713+DIVANSHMAHAJAN@users.noreply.github.com>
2022-01-07 15:05:24 +0530

    Add files via upload

--- src/components/chatsection/ChatContent.js
+++ src/components/chatsection/ChatContent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useContext,useRef} from "react";
 import { useRecoilValue, useRecoilState } from "recoil";
 import {
   chatActiveContact,
@@ -7,40 +7,46 @@ import {
 } from "../../atom/globalState";
 import "./chatContent.css";
 import Avatar from "../ChatPage/Avatar";
+import AuthContext from '../store/auth-context'
 import ChatItem from "./ChatItem";
 import axios from "axios";
 import { message } from "antd";
-
 var stompClient = null;
-var token = null;
-
 const ChatContent = (props) => {
+   const messagesEndRef = useRef(null);
+  const authCtx = useContext(AuthContext);
   const currentUser = useRecoilValue(loggedInUser);
+  const [messagestate,setmessagestate]=useState('');
   const [messages, setMessages] = useRecoilState(chatMessages);
   const [activeContact, setActiveContact] = useRecoilState(chatActiveContact);
   const [contacts, setContacts] = useState([]);
-
+  console.log("activrconrtct");
+  console.log(activeContact.name);
+  console.log("currentUser");
+  console.log(currentUser);
+  useEffect(() => {
+   if (localStorage.getItem("token") !== null) {
+    connect();
+  }
+  }, [authCtx.isLoggedIn])
   useEffect(() => {
     if (localStorage.getItem("token") !== null) {
-      token = localStorage.getItem("token");
-      connect();
-      loadContacts();
-    }
-  }, []);
-
+   setActiveContact({name:props.nameofperson,email:props.email})
+ }
+  }, [props.nameofperson,props.email])
   useEffect(() => {
     if (activeContact === undefined) {
       return;
     }
     const url =
-      "http://localhost:8081/messages/" +
+      "https://chat-lg.azurewebsites.net/messages/" +
       activeContact.id +
       "/" +
       currentUser.id;
     axios
       .get(url, {
         headers: {
-          Authorization: "Bearer" + token,
+          Authorization: "Bearer" + authCtx.token,
         },
       })
       .then((response) => {
@@ -48,14 +54,10 @@ const ChatContent = (props) => {
         setMessages(response.data);
       });
   }, [activeContact]);
-
-// id
-// _id
-
   const connect = () => {
     const Stomp = require("stompjs");
     var SockJS = require("sockjs-client");
-    SockJS = new SockJS("http://localhost:8081/ws");
+    SockJS = new SockJS("https://chat-lg.azurewebsites.net/ws");
     stompClient = Stomp.over(SockJS);
     stompClient.connect({}, onConnected, onError);
   };
@@ -72,7 +74,12 @@ const ChatContent = (props) => {
   const onError = (err) => {
     console.log(err);
   };
-
+  const changeinstate = (e) => {
+    setmessagestate(e.target.value);
+  };
+  
+  console.log("stomp");
+  console.log(stompClient);
   const onMessageReceived = (msg) => {
     const notification = JSON.parse(msg.body);
     const active = JSON.parse(
@@ -81,14 +88,14 @@ const ChatContent = (props) => {
 
     if (active.id === notification.senderId) {
       const url =
-        "http://localhost:8081/messages/" +
+        "https://chat-lg.azurewebsites.net/messages/" +
         currentUser.id +
         "/" +
         notification.id;
       axios
         .get(url, {
           headers: {
-            Authorization: "Bearer" + token,
+            Authorization: "Bearer" + authCtx.token,
           },
         })
         .then((message) => {
@@ -104,92 +111,83 @@ const ChatContent = (props) => {
     }
   };
 
-  const sendMessage = (msg) => {
-    if (msg.trim() !== "") {
+  const sendMessage = () => {
+    if (messagestate.trim() !== "") {
+      console.log("id");
+      console.log(currentUser._id);
       const message = {
-        senderId: currentUser.id,
-        recipientId: activeContact.id,
-        senderName: currentUser.name,
+        senderId: currentUser.username,
+        recipientId: activeContact.email,
+        senderName: currentUser.firstName,
         recipientName: activeContact.name,
-        content: msg,
+        content: messagestate,
         timestamp: new Date(),
       };
-      stompClient.send("/app/chat", {}, JSON.stringify(message));
+     stompClient.send("/app/chat", {}, JSON.stringify(message));
+     console.log(message);
       const newMessages = [...messages];
       newMessages.push(message);
       setMessages(newMessages);
     }
   };
 
-  const loadContacts = () => {
-    axios
-      .get("http://localhost:8081/users/", {
-        headers: {
-          Authorization: "Bearer" + token,
-        },
-      })
-      .then((response) => {
-        setContacts(response.data);
-      });
-  };
+ 
 
-  return <div>Hello</div>;
+  return (
+ <div className="main__chatcontent">
+     <div className="content__header">
+       <div className="blocks">
+        <div className="current-chatting-user">
+          <Avatar
+           isOnline="active"
+           image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU"
+          />
+           <p>{props.nameofperson}</p>
+        </div>
+       </div>
 
-  // return (
-  // <div className="main__chatcontent">
-  //   <div className="content__header">
-  //     <div className="blocks">
-  //       <div className="current-chatting-user">
-  //         <Avatar
-  //           isOnline="active"
-  //           image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU"
-  //         />
-  //         <p>{props.nameofperson}</p>
-  //       </div>
-  //     </div>
-
-  //     <div className="blocks">
-  //       <div className="settings">
-  //         <button className="btn-nobg">
-  //           <i className="fa fa-cog"></i>
-  //         </button>
-  //       </div>
-  //     </div>
-  //   </div>
-  //   <div className="content__body">
-  //     <div className="chat__items">
-  //       {chatstate.map((itm, index) => {
-  //         return (
-  //           <ChatItem
-  //             animationDelay={index + 2}
-  //             key={index}
-  //             user={itm.type ? itm.type : "me"}
-  //             msg={itm.msg}
-  //             image={itm.image}
-  //           />
-  //         );
-  //       })}
-  //       <div ref={messagesEndRef} />
-  //     </div>
-  //   </div>
-  //   <div className="content__footer">
-  //     <div className="sendNewMessage">
-  //       <button className="addFiles">
-  //         <i className="fa fa-plus"></i>
-  //       </button>
-  //       <input
-  //         type="text"
-  //         placeholder="Type a message here"
-  //         onChange={changeinstate}
-  //         value={messagestate}
-  //       />
-  //       <button className="btnSendMsg" id="sendMsgBtn" onClick={sendMessage}>
-  //         <i className="fa fa-paper-plane"></i>
-  //       </button>
-  //     </div>
-  //   </div>
-  // </div>
-  // );
-};
+       <div cla ssName="blocks">
+         <div className="settings">
+          <button className="btn-nobg">
+             <i className="fa fa-cog"></i>
+           </button>
+         </div>
+       </div>
+     </div>
+     <div className="content__body">
+       <div className="chat__items">
+         {messages.map((itm, index) => {
+           return (
+          <ChatItem
+         animationDelay={index + 2}
+               key={index}
+               user={itm.type ? itm.type : "me"}
+               msg={itm.content}
+               
+            />
+          );
+         })}
+         <div ref={messagesEndRef} />
+       </div>
+     </div>
+     <div className="content__footer">
+       <div className="sendNewMessage">
+         <button className="addFiles">
+           <i className="fa fa-plus"></i>
+         </button>
+         <input
+           type="text"
+          placeholder="Type a message here"
+           onChange={changeinstate}
+           value={messagestate}
+        />
+        <button className="btnSendMsg" id="sendMsgBtn" onClick={sendMessage}>
+           <i className="fa fa-paper-plane"></i>
+         </button>
+       </div>
+     </div>
+   </div>
+   );
+ };
 
 export default ChatContent;

e3eba4fad716e6979a6399390490e07e4d6b889c
divansh <divanshmahajan09@gmail.com>
2022-01-29 16:30:08 +0530

    latest index

--- src/index.js
+++ src/index.js
@@ -16,10 +16,9 @@ const { RecoilPersist, updateState } = recoilPersist([], {
   key: "recoil-persist",
   storage: sessionStorage,
 });
-const lang=process.env.lang||'en-US'
-const appid=process.env.app_id||'8b238cd8-597a-4c70-add6-222799409645'
+
 ReactDOM.render(
-  <SpeechProvider appId={env.appid} language={env.lang}>
+  <SpeechProvider appId={env.app_id} language={env.lang}>
   <AuthContextProvider>
     <BrowserRouter>
       <RecoilRoot initializeState={updateState}>

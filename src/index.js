588332f5fadd669bb52872f56c95096866fb2bac
divansh <divanshmahajan09@gmail.com>
2022-01-29 16:12:25 +0530

    heroku env add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== "production")
+{
+  require('dotenv').config();
+}
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -14,9 +18,10 @@ const { RecoilPersist, updateState } = recoilPersist([], {
   key: "recoil-persist",
   storage: sessionStorage,
 });
-
+const lang=process.env.lang||'en-US'
+const appid=process.env.app_id||'8b238cd8-597a-4c70-add6-222799409645'
 ReactDOM.render(
-  <SpeechProvider appId="8b238cd8-597a-4c70-add6-222799409645" language="en-US">
+  <SpeechProvider appId={appid} language={lang}>
   <AuthContextProvider>
     <BrowserRouter>
       <RecoilRoot initializeState={updateState}>
